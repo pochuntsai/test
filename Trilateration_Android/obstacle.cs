8cfac38ffb83cc4a6ae39a612c625576b3e3cb0b
Brian <pochun.tsai@gmail.com>
2015-03-27 14:14:00 +0800

    Merge toby's source (Ver_A1)

--- Trilateration_Android/obstacle.cs
+++ Trilateration_Android/obstacle.cs
@@ -7,27 +7,29 @@ namespace Trilateration
 {
     class obstacle
     {
+
         // constant value
         private int d_max = 150;
-        private int d_safe = 35;
+        private int[] d_safe = new int[5] { 15, 30, 35, 30, 15 };
         private int d_danger = 15;
         private int speed_normal = 25;
         private int sonic_number = 5;
         private Single kp = 0.8f;
         private int ku = 16; //32
+
         // need to know at the beginning
         private int turn_max;
         private Single cycle_interval;
 
         // private variables
         private int i;
-        private int[] sonic=new int[5];
-        private int speed;
-        private int turn;
-        private int d_shortest;
-        private int d_shortest_old;
+        private int[] sonic;
+        private int[] sonic_old;
+        private Single[] cost;
         private Single d_delta;
         private Single err_delta;
+        private int speed;
+        private int turn;
         private Single u;
         private Single tmpSingle;
 
@@ -58,101 +60,102 @@ namespace Trilateration
         {
             get { return hasObstacle; }
         }
-        public Single Delta_error
-        {
-            get { return err_delta; }
-        }
-        public Single Delta_D
-        {
-            get { return d_delta; }
-        }
         public Single Control
         {
             get { return u; }
         }
 
+        /// <summary>
+        /// To create an object from this class
+        /// </summary>
+        /// <param name="var1">input the max turning value</param>
+        /// <param name="var2">input the duration of each cycle (in second)</param>
         public obstacle(int var1,Single var2)
         {
             turn_max = var1;
             cycle_interval = var2;
             hasObstacle = false;
             stop = false;
-            d_shortest = d_safe;
-            d_shortest_old = d_safe;
-            d_delta = 0;
-            err_delta = 0;
             u = 0;
             turn = 0;
+            sonic = new int[sonic_number];
+            sonic_old = new int[sonic_number];
+            cost = new Single[sonic_number];
+            d_delta = 0;
+            err_delta = 0;
+            for (i = 0; i < sonic_number; i++)
+            {
+                sonic[i] = d_max;
+                sonic_old[i] = d_max;
+                cost[i] = 1;
+            }
         }
 
+        /// <summary>
+        /// To save the sensor readings and determine if there's any obstacle
+        /// </summary>
+        /// <param name="readings">The number of elements in the array has to be same as setting</param>
         public void save_sensor_reading(byte[] readings)
         {
             hasObstacle = false;
             for (i = 0; i < sonic_number; i++)
             {
+                sonic_old[i] = sonic[i];
                 sonic[i] = (int)readings[i];
-                if (sonic[i] < d_safe) hasObstacle = true;
+                if (sonic[i] < d_safe[i]) hasObstacle = true;
+
+                cost[i] = (Single)sonic[i] / d_safe[i];
+                if (cost[i] > 1) cost[i] = 1;
             }
         }
 
+        /// <summary>
+        /// If there's an obstacle, initiate avoidance mechanism
+        /// </summary>
+        /// <param name="origin_turn">Give the current turning command as reference</param>
         public void avoid(int origin_turn)
         {
+            int index_nearest;
             Single ss;
             Single cost_R, cost_L;
 
             turn = origin_turn;
 
             // calculate the cost of right and left
-            cost_L = 1 / ((sonic[0] + sonic[1] + sonic[2]+1) / 3f);
-            cost_R = 1 / ((sonic[2] + sonic[3] + sonic[4]+1) / 3f);
+            cost_L = (cost[0] + cost[1] + cost[2]) / 3f;
+            cost_R = (cost[2] + cost[3] + cost[4]) / 3f;
 
-            // find the shortest distance between the obstacle and the vehicle
+            index_nearest = 0;
             for (i = 0; i < sonic_number; i++)
             {
-                if (d_shortest > sonic[i]) d_shortest = sonic[i];
+                if (cost[i] < cost[index_nearest])
+                {
+                    index_nearest = i;
+                }
             }
 
-            // check if the shortest distance is away from the safe zone
-            if (d_shortest >= d_safe)
-            {
-                hasObstacle = false;
-            }
 
-            d_delta = (Single)(5)*((d_shortest - d_shortest_old) / cycle_interval) / (d_safe / cycle_interval);
-            err_delta = 1f * (d_safe - d_shortest) / d_safe;
+            d_delta = (Single)(5)*((sonic[index_nearest] - sonic_old[index_nearest]) / cycle_interval) / (d_safe[index_nearest] / cycle_interval);
+            err_delta = 1f * (d_safe[index_nearest] - sonic[index_nearest]) / d_safe[index_nearest];
             ss = kp * (err_delta + d_delta);
-            u = Fuzzy(ss);
-        //    if (d_delta >= 0)
-         //   {
-                if (cost_L >= cost_R)
-                {
-                    right = true;
-                    left = false;
-                    stop = false;
-                    speed = speed_normal;
-                }
-                else
-                {
-                    right = false;
-                    left = true;
-                    stop = false;
-                    speed = speed_normal;
-                }
-                tmpSingle = turn + u * 5f *ku;
-                turn = (int)tmpSingle;
-           /* }
-            else
+            
+            if (cost_L >= cost_R)
             {
                 right = false;
+                left = true;
+                stop = false;
+                speed = speed_normal;
+            }
+            else
+            {
+                right = true;
                 left = false;
-                stop = true;
-                speed = 18;
-                //turn = (int)(turn + 5);
-                turn = 1 * turn;
-            }*/
-
-            // update old distance
-            d_shortest_old = d_shortest;
+                stop = false;
+                speed = speed_normal;
+            }
+            u = Fuzzy(ss);
+            tmpSingle = turn + u * 5f *ku;
+            turn = (int)tmpSingle;
             
         }
 
