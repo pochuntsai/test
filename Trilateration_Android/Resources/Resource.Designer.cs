28064298c3d321dc133ff9733b017c2e7852b29c
Brian <pochun.tsai@gmail.com>
2015-03-31 15:33:55 +0800

    1. Enable "calcompass" button for e-compass calabration 2. Enable "labelVehicle" textview to show e-compass value

--- Trilateration_Android/Resources/Resource.Designer.cs
+++ Trilateration_Android/Resources/Resource.Designer.cs
@@ -63,17 +63,17 @@ namespace Trilateration_Android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f05000c
-			public const int btnConnect = 2131034124;
-			
 			// aapt resource value: 0x7f05000d
-			public const int btnDelete = 2131034125;
+			public const int btnConnect = 2131034125;
 			
 			// aapt resource value: 0x7f05000e
-			public const int btnGo = 2131034126;
+			public const int btnDelete = 2131034126;
 			
-			// aapt resource value: 0x7f050007
-			public const int btnLoad = 2131034119;
+			// aapt resource value: 0x7f05000f
+			public const int btnGo = 2131034127;
+			
+			// aapt resource value: 0x7f050008
+			public const int btnLoad = 2131034120;
 			
 			// aapt resource value: 0x7f050003
 			public const int buttonCalCompass = 2131034115;
@@ -84,38 +84,41 @@ namespace Trilateration_Android
 			// aapt resource value: 0x7f050002
 			public const int buttonTest2 = 2131034114;
 			
-			// aapt resource value: 0x7f050006
-			public const int labelA = 2131034118;
-			
-			// aapt resource value: 0x7f050008
-			public const int labelB = 2131034120;
+			// aapt resource value: 0x7f050007
+			public const int labelA = 2131034119;
 			
 			// aapt resource value: 0x7f050009
-			public const int labelC = 2131034121;
+			public const int labelB = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int labelC = 2131034122;
 			
 			// aapt resource value: 0x7f050004
 			public const int labelTable = 2131034116;
 			
-			// aapt resource value: 0x7f05000a
-			public const int labelTag = 2131034122;
+			// aapt resource value: 0x7f050005
+			public const int labelTableC = 2131034117;
 			
 			// aapt resource value: 0x7f05000b
-			public const int labelVehicle = 2131034123;
+			public const int labelTag = 2131034123;
 			
-			// aapt resource value: 0x7f050005
-			public const int linearContent = 2131034117;
+			// aapt resource value: 0x7f05000c
+			public const int labelVehicle = 2131034124;
+			
+			// aapt resource value: 0x7f050006
+			public const int linearContent = 2131034118;
 			
 			// aapt resource value: 0x7f050000
 			public const int linearLayout1 = 2131034112;
 			
-			// aapt resource value: 0x7f050010
-			public const int scrollView1 = 2131034128;
-			
-			// aapt resource value: 0x7f05000f
-			public const int spinnerTarget = 2131034127;
-			
 			// aapt resource value: 0x7f050011
-			public const int textView = 2131034129;
+			public const int scrollView1 = 2131034129;
+			
+			// aapt resource value: 0x7f050010
+			public const int spinnerTarget = 2131034128;
+			
+			// aapt resource value: 0x7f050012
+			public const int textView = 2131034130;
 			
 			static Id()
 			{
